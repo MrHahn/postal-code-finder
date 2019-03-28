7fa85c21a4bfb85cc19e09b867514178864e2021
MrHahn <chahn970@gmail.com>
2019-03-27 21:19:36 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($){
 	$('#submitPost').click(function(){
 		$.ajax({
 			type: 'GET',
-			url:'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyDYgc6ZSNkQnifUjrrCBEWxY-pZwsER38U',
+			url:'https://maps.googleapis.com/maps/api/geocode/xml?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyDYgc6ZSNkQnifUjrrCBEWxY-pZwsER38U',
 			dataType: 'xml',
 			success: processXML
 		});
