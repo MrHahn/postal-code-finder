01eb54521027ba344d4e2ea6598f6627408fada0
MrHahn <chahn970@gmail.com>
2019-03-27 19:57:59 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 jQuery(document).ready(function($){
 	$('#submitPost').click(function(){
-		$.ajax({
-			type: 'GET',
-			url:'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyDYgc6ZSNkQnifUjrrCBEWxY-pZwsER38U',
-			dataType: 'json',
-			success: processJson
+		// $.ajax({
+		// 	type: 'GET',
+		// 	url:'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyDYgc6ZSNkQnifUjrrCBEWxY-pZwsER38U',
+		// 	dataType: 'json',
+		// 	success: processJson
+		// });
+		 $.ajax({
+        url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyDYgc6ZSNkQnifUjrrCBEWxY-pZwsER38U'
+		    }).then(function(json) {
+		      alert(json.address_components.long_name);
 		});
 
-		function processJson(json){
-			alert($(json).find('address_component').text());
-			alert(json.address_component);	
-		}
+
+		// function processJson(json){
+		// 	alert($(json).find('address_component').text());
+		// 	alert(json.address_component.long_address);	
+		// }
 	})
 })
